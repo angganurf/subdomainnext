db6edc137c0aa47d99dd4c6496318a4fb980eae9
Angga Nurf <31128877+angganurf@users.noreply.github.com>
2024-05-15 22:51:55 +0700

    Update page.tsx

--- app/[domain]/page.tsx
+++ app/[domain]/page.tsx
@@ -1,54 +1,17 @@
-import Link from "next/link";
-import prisma from "@/lib/prisma";
-import { notFound } from "next/navigation";
-import BlurImage from "@/components/blur-image";
-import { placeholderBlurhash, toDateString } from "@/lib/utils";
-import BlogCard from "@/components/blog-card";
-import { getPostsForSite, getSiteData } from "@/lib/fetchers";
-import Image from "next/image";
-import { permanentRedirect } from 'next/navigation';
+"use client";
+import { useEffect } from "react";
 
-
-export async function generateStaticParams() {
-  const allSites = await prisma.site.findMany({
-    select: {
-      subdomain: true,
-      customDomain: true,
-    },
-    // feel free to remove this filter if you want to generate paths for all sites
-    where: {
-      subdomain: "demo",
-    },
-  });
-
-  const allPaths = allSites
-    .flatMap(({ subdomain, customDomain }) => [
-      subdomain && {
-        domain: `${subdomain}.${process.env.NEXT_PUBLIC_ROOT_DOMAIN}`,
-      },
-      customDomain && {
-        domain: customDomain,
-      },
-    ])
-    .filter(Boolean);
-
-  return allPaths;
-}
-
-export default async function SiteHomePage({
-  params,
-}: {
-  params: { domain: string };
-}) {
-  const domain = decodeURIComponent(params.domain);
-  const [data, posts] = await Promise.all([
-    getSiteData(domain),
-    getPostsForSite(domain),
-  ]);
-
-  if (!data) {
-    notFound();
-  }
-
-  return permanentRedirect('https://facebook.com');
+export default function Page() {
+  useEffect(() => {
+    if (window.location) window.location.replace("https://youtube.com");
+  }, []);
+  const override = `
+    display: block;
+    margin: 0 auto;
+  `;
+  return (
+    <>
+      <p className="text-center">Redirecting...</p>
+    </>
+  );
 }
